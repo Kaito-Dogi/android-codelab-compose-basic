40fb866c5f1278221ccb046101dde32513e17dd0
Kaito-Dogi <kendobu0405@gmail.com>
2022-04-27 02:37:56 +0900

    first commit

---
+++ app/src/main/java/com/doggy/android_codelab_compose_basic/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.doggy.android_codelab_compose_basic.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
