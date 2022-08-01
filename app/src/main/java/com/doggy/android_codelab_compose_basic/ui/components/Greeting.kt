717f99fb9c1cb92664077be71c708603503ef449
Kaito Dogi <kendobu0405@gmail.com>
2022-08-02 01:37:45 +0900

    feat: Greeting に状態 expand を追加

--- app/src/main/java/com/doggy/android_codelab_compose_basic/ui/components/Greeting.kt
+++ app/src/main/java/com/doggy/android_codelab_compose_basic/ui/components/Greeting.kt
@@ -2,18 +2,20 @@ package com.doggy.android_codelab_compose_basic.ui.components
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 internal fun Greeting(name: String) {
+  val expand = remember { mutableStateOf(false) }
   Surface(
     color = MaterialTheme.colors.primary,
     modifier = Modifier
@@ -28,9 +30,9 @@ internal fun Greeting(name: String) {
         Text(text = "${name}!")
       }
       OutlinedButton(
-        onClick = { /*TODO*/ }
+        onClick = { expand.value = !expand.value },
       ) {
-        Text(text = "Show More")
+        Text(text = if (expand.value) "Show Less" else "Show More")
       }
     }
   }
