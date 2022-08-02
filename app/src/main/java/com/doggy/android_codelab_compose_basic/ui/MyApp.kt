e649c5111cf15a5c36f7497e014018df4495f5f2
Kaito Dogi <kendobu0405@gmail.com>
2022-08-03 01:08:52 +0900

    refac: remember --> rememberSaveavle

--- app/src/main/java/com/doggy/android_codelab_compose_basic/ui/MyApp.kt
+++ app/src/main/java/com/doggy/android_codelab_compose_basic/ui/MyApp.kt
@@ -3,14 +3,14 @@ package com.doggy.android_codelab_compose_basic.ui
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import com.doggy.android_codelab_compose_basic.ui.screens.GreetingsScreen
 import com.doggy.android_codelab_compose_basic.ui.screens.OnBoardingScreen
 
 @Composable
 internal fun MyApp() {
-  var shouldShowOnBoarding by remember { mutableStateOf(true) }
+  var shouldShowOnBoarding by rememberSaveable { mutableStateOf(true) }
 
   if (shouldShowOnBoarding) {
     OnBoardingScreen(onContinueClicked = { shouldShowOnBoarding = false })
