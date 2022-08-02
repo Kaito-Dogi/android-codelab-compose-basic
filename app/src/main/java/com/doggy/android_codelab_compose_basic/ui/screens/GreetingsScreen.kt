3b33da7d0c02ed1e67e9d03d5455fdbff24d0472
Kaito Dogi <kendobu0405@gmail.com>
2022-08-03 01:02:50 +0900

    feat: LazyColumn を追加

--- app/src/main/java/com/doggy/android_codelab_compose_basic/ui/screens/GreetingsScreen.kt
+++ app/src/main/java/com/doggy/android_codelab_compose_basic/ui/screens/GreetingsScreen.kt
@@ -1,8 +1,9 @@
 package com.doggy.android_codelab_compose_basic.ui.screens
 
 import android.content.res.Configuration
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -12,9 +13,9 @@ import com.doggy.android_codelab_compose_basic.ui.components.Greeting
 import com.doggy.android_codelab_compose_basic.ui.theme.AndroidcodelabcomposebasicTheme
 
 @Composable
-internal fun GreetingsScreen(names: List<String> = listOf("Android", "Compose")) {
-  Column(modifier = Modifier.padding(vertical = 4.dp)) {
-    for (name in names) {
+internal fun GreetingsScreen(names: List<String> = List(1000) { "$it" }) {
+  LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+    items(items = names) { name ->
       Greeting(name = name)
     }
   }
